ad8e53c5eb67577ff01e2c6ada666808ac8d947d
nishihara-e <nishihara.e.aa@m.titech.ac.jp>
2019-06-30 03:05:59 +0900

    kansei

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -18,7 +18,22 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
-  
+  int i, j, pivot;
+  if(n<=1){
+    return 0;
+  }
+  pivot = A[n/2];
+  A[n/2] = A[0];
+  A[0] = pivot;
+  for(i = j = 1; i < n; i++){
+    if(A[i] <= pivot){
+      swap(A+i, A+j);
+      j++;
+    }
+  }
+  swap(A,A+j-1);
+  quick_sort(A,j-1);
+  quick_sort(A+j,n-j);
 }
 
 int main(){
